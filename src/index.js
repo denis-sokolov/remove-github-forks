2b1f621a5b7295b2050e2ff20ebbc5049da16a22
Denis Sokolov <denis@sokolov.cc>
2016-07-24 11:22:55 +0300

    Queue requests to GitHub

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 var async = require('async')
 var GithubLib = require('github')
+var Promise = require('promise')
+var queue = require('queue')
 
 var shouldDeleteFork = require('./shouldDeleteFork')
 
-var githubFactory = function (token) {
+var rawGithubFactory = function (token) {
   // Allow to inject a GitHub API instead of a token
   if (token.repos) {
     return token
@@ -19,6 +21,53 @@ var githubFactory = function (token) {
     token: token
   })
   return res
+}
+
+var githubFactory = function (token) {
+  var api = rawGithubFactory(token)
+
+  var q = queue({
+    concurrency: 1,
+    timeout: 2000
+  })
+
+  // Transform a github function call into a queued function call
+  // to ensure that only one API call runs at a time
+  // https://developer.github.com/guides/best-practices-for-integrators/#dealing-with-abuse-rate-limits
+  var qd = function(call){
+    return function(){
+      var args = [].slice.apply(arguments)
+      return new Promise(function(resolve, reject){
+        q.push(function(cb){
+          var argsCb = args.pop()
+          if (typeof argsCb !== 'function') {
+            args.push(argsCb)
+            argsCb = null
+          }
+          call.apply(null, args).then(function(result){
+            cb()
+            if (argsCb) argsCb(null, result)
+            resolve(result)
+          }, function(error){
+            cb()
+            if (argsCb) argsCb(error)
+            reject(error)
+          })
+        })
+        q.start()
+      })
+    }
+  }
+
+  return {
+    repos: {
+      compareCommits: qd(api.repos.compareCommits),
+      delete: qd(api.repos.delete),
+      get: qd(api.repos.get),
+      getAll: qd(api.repos.getAll),
+      getBranches: qd(api.repos.getBranches)
+    }
+  }
 }
 
 var api = function (token, cb) {
