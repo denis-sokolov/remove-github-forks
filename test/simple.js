0a4c09123b684d3b3d44c6c2bc0ece11156b3375
Denis Sokolov <denis@sokolov.cc>
2016-10-10 17:28:20 +0300

    update github to version 5.0.0

--- test/simple.js
+++ test/simple.js
@@ -11,10 +11,10 @@ test.cb('should delete a fork with no branches', function(t){
     removeGithubForks(mock.present, function(){
         lib.check(t, mock.calls(), [
           [ 'getAll', { per_page: 100, type: 'public' } ],
-          [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-          [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ],
-          [ 'getBranches', { user: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
-          [ 'delete', { user: lib.USER.login, repo: 'fork1', url: undefined } ]
+          [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+          [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ],
+          [ 'getBranches', { owner: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
+          [ 'delete', { owner: lib.USER.login, repo: 'fork1', url: undefined } ]
         ])
         t.end()
     })
@@ -29,8 +29,8 @@ if (String.prototype.repeat)
         removeGithubForks(mock.present, function(){
             lib.check(t, mock.calls(), [
               [ 'getAll', { per_page: 100, type: 'public' } ],
-              [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-              [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ]
+              [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+              [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ]
             ])
             t.end()
         })
@@ -39,7 +39,7 @@ if (String.prototype.repeat)
 test.cb('should not delete a fork that has more branches', function(t){
     var mock = lib.mock({
         getBranches: function(args){
-            if (args.user === lib.USER.login)
+            if (args.owner === lib.USER.login)
                 return ['foo'];
             return [];
         }
@@ -48,9 +48,9 @@ test.cb('should not delete a fork that has more branches', function(t){
     removeGithubForks(mock.present, function(){
         lib.check(t, mock.calls(), [
           [ 'getAll', { per_page: 100, type: 'public' } ],
-          [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-          [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ],
-          [ 'getBranches', { user: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ]
+          [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+          [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ],
+          [ 'getBranches', { owner: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ]
         ])
         t.end()
     })
@@ -77,10 +77,10 @@ test.skip('should delete a fork that has more branches, but all at upstream bran
     removeGithubForks(mock.present, function(){
         lib.check(t, mock.calls(), [
           [ 'getAll', { per_page: 100, type: 'public' } ],
-          [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-          [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ],
-          [ 'getBranches', { user: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
-          [ 'delete', { user: lib.USER.login, repo: 'fork1', url: undefined } ]
+          [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+          [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ],
+          [ 'getBranches', { owner: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
+          [ 'delete', { owner: lib.USER.login, repo: 'fork1', url: undefined } ]
         ])
         t.end()
     })
@@ -105,10 +105,10 @@ test.cb('should delete a fork that has all branches at upstream branch tips', fu
     removeGithubForks(mock.present, function(){
         lib.check(t, mock.calls(), [
           [ 'getAll', { per_page: 100, type: 'public' } ],
-          [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-          [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ],
-          [ 'getBranches', { user: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
-          [ 'delete', { user: lib.USER.login, repo: 'fork1', url: undefined } ]
+          [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+          [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ],
+          [ 'getBranches', { owner: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
+          [ 'delete', { owner: lib.USER.login, repo: 'fork1', url: undefined } ]
         ])
         t.end()
     })
@@ -117,7 +117,7 @@ test.cb('should delete a fork that has all branches at upstream branch tips', fu
 test.cb('should delete a fork that has branches behind', function(t){
     var mock = lib.mock({
         getBranches: function(args){
-            if (args.user === lib.USER.login)
+            if (args.owner === lib.USER.login)
                 return [
                     { name: 'master', commit: lib.COMMIT_A },
                     { name: 'foo', commit: lib.COMMIT_B }
@@ -134,13 +134,13 @@ test.cb('should delete a fork that has branches behind', function(t){
     removeGithubForks(mock.present, function(){
         lib.check(t, mock.calls(), [
           [ 'getAll', { per_page: 100, type: 'public' } ],
-          [ 'get', { user: lib.USER.login, repo: 'fork1' } ],
-          [ 'getBranches', { user: lib.USER.login, repo: 'fork1', per_page: 100 } ],
-          [ 'getBranches', { user: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
-          [ 'compareCommits', { user: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_B.sha, head: 'master' } ],
-          [ 'compareCommits', { user: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_B.sha, head: 'foo' } ],
-          [ 'compareCommits', { user: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_A.sha, head: 'master' } ],
-          [ 'delete', { user: lib.USER.login, repo: 'fork1', url: undefined } ]
+          [ 'get', { owner: lib.USER.login, repo: 'fork1' } ],
+          [ 'getBranches', { owner: lib.USER.login, repo: 'fork1', per_page: 100 } ],
+          [ 'getBranches', { owner: lib.AUTHOR.login, repo: 'upstream-lib', per_page: 100 } ],
+          [ 'compareCommits', { owner: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_B.sha, head: 'master' } ],
+          [ 'compareCommits', { owner: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_B.sha, head: 'foo' } ],
+          [ 'compareCommits', { owner: lib.AUTHOR.login, repo: 'upstream-lib', base: lib.COMMIT_A.sha, head: 'master' } ],
+          [ 'delete', { owner: lib.USER.login, repo: 'fork1', url: undefined } ]
         ])
         t.end()
     })
